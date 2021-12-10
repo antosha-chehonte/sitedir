0dfb2d40866b54d237400b873cd41b93350e9167
Черкашин Антон <anton_cherkashin@list.ru>
2021-12-10 18:05:26 +0700

    первый коммит, на сервере, приложение news, новости не добавляет

---
+++ news/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.urls import reverse
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок объявления')
+    content = RichTextUploadingField(blank=False, verbose_name='Содержание объявления')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    # file = models.FileField(upload_to='files/%Y/%m/%d/', verbose_name='Прикрепить файл', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    show_author = models.BooleanField(default=True, verbose_name='Показать автора объявления')
+    pinned = models.BooleanField(default=False, verbose_name='Закрепить сообщение')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT,
+                                 null=True, verbose_name='Направление деятельности')
+    style = models.ForeignKey('Style', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Стиль оформления')
+
+    # def filename(self):
+    #     return os.path.basename(self.file.name)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('news_view', kwargs={"news_id": self.pk})
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование направления работы')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Направление работы'
+        verbose_name_plural = 'Направления работы'
+        ordering = ['title']
+
+
+class Style(models.Model):
+    style = models.CharField(max_length=100, verbose_name='Стиль оформления')
+    style_tag = models.CharField(max_length=100, verbose_name='Tag')
+
+    def __str__(self):
+        return self.style
+
+    class Meta:
+        verbose_name = 'Стиль оформления'
+        verbose_name_plural = 'Стили оформления'
+        ordering = ['style']
+
+
+class Files(models.Model):
+    file = models.FileField(upload_to='files/%Y/%m/%d/', blank=True, null=True, verbose_name='Файл')
+    contact = models.ForeignKey(News, blank=True, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Файлы"
+        verbose_name_plural = "Файлы"
+
+    def __str__(self):
+        return self.file.name
---
+++ news/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from news.views import index
+
+urlpatterns = [
+    path('', index, name='index')
+]
