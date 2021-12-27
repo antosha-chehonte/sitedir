01e15140f96dac2293a1875ee9afaad98f80f3f6
Черкашин Антон <anton_cherkashin@list.ru>
2021-12-27 19:56:37 +0700

    приложения: news, actual; настройка приложения actual

--- actual/views.py
+++ actual/views.py
@@ -4,7 +4,7 @@ from actual.models import DirectionsList, Notes
 
 def actual_index(request, parent_id=1):
     direction = DirectionsList.objects.filter(parent=parent_id)
-    notes = Notes.objects.filter(direction_of_work=parent_id)
+    notes = Notes.objects.filter(direction_of_work=parent_id).order_by('-updated_at')
     current_direction = DirectionsList.objects.get(pk=parent_id)
     if not parent_id:
         parent = None
---
+++ news/forms.py
@@ -0,0 +1,26 @@
+from ckeditor.widgets import CKEditorWidget
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+from .models import News
+
+
+class NewsEditForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget, label='')
+
+    class Meta:
+        model = News
+        fields = [
+            'category',
+            'title',
+            'content',
+            # 'file',
+            'style',
+            'show_author',
+            'is_published',
+        ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'style': forms.Select(attrs={'class': 'form-control', 'style': 'width:460px'}),
+        }
