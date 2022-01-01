e1642cbcb1a219b754d6b626148ed2568c083dd1
Черкашин Антон <anton_cherkashin@list.ru>
2022-01-01 20:58:03 +0700

    actual: кнопка создания папки, url редактирования заметки, view редактирования заметки, иконка редактирования заметки, base_actual добавлена онлайн ссылка на fontawesome, создан шаблон admin (вью нет), в шаблоне редактирования заметки кнопка "назад",

--- actual/urls.py
+++ actual/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from actual.views import actual_index, note_add
+from actual.views import actual_index, note_add, note_edit
 
 urlpatterns = [
     path('', actual_index, name='actual_index'),
     path('directions/<int:parent_id>', actual_index, name='actual_directions'),
     path('add/', note_add, name='note_add'),  # добавить заметку
+    path('edit/<int:note_id>', note_edit, name='note_edit'),  # конкретная заметка
 ]
--- actual/views.py
+++ actual/views.py
@@ -31,10 +31,31 @@ def note_add(request):
         if post_form.is_valid():
             post_form.save()
             # return redirect(note_add)
-            # return render(request, 'actual/note_add.html')
+            return render(request, 'actual/note_add.html')
     else:
         post_form = NoteEditForm
         context = {
             "post_form": post_form,
+        }
+        return render(request, 'actual/note_add.html', context)
+
+
+@login_required
+def note_edit(request, note_id):
+    editable_news = Notes.objects.get(pk=note_id)
+    editable = True
+    revers_id = editable_news.direction_of_work.pk
+    if request.method == 'POST':
+        post_form = NoteEditForm(request.POST, instance=editable_news)
+        if post_form.is_valid():
+            post_form.save()
+            # return redirect(index)
+    else:
+        post_form = NoteEditForm(instance=editable_news)
+        context = {
+            "post_form": post_form,
+            "editable": editable,
+            "revers_id": revers_id,
+
         }
         return render(request, 'actual/note_add.html', context)
